ffd5e532d136d56d8e007f6e73f11ecf1f8017ee
Paras Buda <parasbuda1234@gmail.com>
2020-08-11 17:25:56 +0545

    navbar is finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React from "react"
+import Header from "./Component/Header/Header"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import "./App.css"
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <div>
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+        <Route exact path="/"><h1>Homepage Component</h1></Route>
+        <Route path="/login"><h1>Login Component</h1></Route>
+        <Route path="/logout"><h1>Logout Component</h1></Route>
+        <Route path="/checkout"><h1>Checkout Component</h1></Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
 }
 
-export default App;
+export default App
