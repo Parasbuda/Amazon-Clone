392bb354fef7aebd6cf1541cae230c13b58a54fe
Paras Buda <parasbuda1234@gmail.com>
2020-08-12 11:11:09 +0545

    navbar is sticky made

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { BrowserRouter, Switch, Route } from "react-router-dom"
 import "./App.css"
 
 function App() {
-  return <div>
-    <BrowserRouter>
-      <Header/>
-      <Switch>
-        <Route exact path="/"><h1>Homepage Component</h1></Route>
-        <Route path="/login"><h1>Login Component</h1></Route>
-        <Route path="/logout"><h1>Logout Component</h1></Route>
-        <Route path="/checkout"><h1>Checkout Component</h1></Route>
-      </Switch>
-    </BrowserRouter>
-  </div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h1>Homepage Component</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Component</h1>
+          </Route>
+          <Route path="/logout">
+            <h1>Logout Component</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>Checkout Component</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
