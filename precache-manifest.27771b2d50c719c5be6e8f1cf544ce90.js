b891025d81d04cc97e227bebf99cfc7f879b399a
Parasbuda <60026182+Parasbuda@users.noreply.github.com>
2022-01-24 10:22:51 +0000

    Deploying to gh-pages from @ Parasbuda/Amazon-Clone@ce45ab335443d815fcb9eb27bcb07a81d0df3130 🚀

---
+++ precache-manifest.27771b2d50c719c5be6e8f1cf544ce90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5994e57b476c237061daffbdeaaa3153",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "1dc05bce43ced7e5f959",
+    "url": "/amazon-clone/static/css/main.e0e5f020.chunk.css"
+  },
+  {
+    "revision": "29617be4804454c0c0bf",
+    "url": "/amazon-clone/static/js/2.2cc7f609.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.2cc7f609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc05bce43ced7e5f959",
+    "url": "/amazon-clone/static/js/main.b77fc114.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
